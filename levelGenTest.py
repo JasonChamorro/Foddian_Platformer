64eec760ad81173038511d95a23707c5305b012c
Mrazo523 <marcuskrazo@gmail.com>
2023-11-28 14:45:20 -0800

    Adding levelGenTest file

---
+++ levelGenTest.py
@@ -0,0 +1,264 @@
+import pygame
+import os
+import random
+import csv
+
+pygame.init()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Shooter')
+
+clock = pygame.time.Clock()
+FPS = 60
+
+GRAVITY = 1.2
+ROWS = 16
+COLS = 26
+TILE_SIZE = SCREEN_HEIGHT // ROWS
+TILE_TYPES = 21
+level = 1
+
+moving_left = False
+moving_right = False
+
+img_list = []
+for x in range(TILE_TYPES):
+	img = pygame.image.load(f'img/Tile/{x}.png')
+	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+	img_list.append(img)
+
+
+BG = (144, 201, 120)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+
+
+def draw_bg():
+	screen.fill(BG)
+
+
+class Soldier(pygame.sprite.Sprite):
+	def __init__(self, char_type, x, y, scale, speed, ammo, grenades):
+		pygame.sprite.Sprite.__init__(self)
+		self.char_type = char_type
+		self.speed = speed
+
+		self.direction = 1
+		self.vel_y = 0
+		self.jump = False
+		self.in_air = True
+		self.flip = False
+		self.animation_list = []
+		self.frame_index = 0
+		self.action = 0
+		self.update_time = pygame.time.get_ticks()
+		
+		animation_types = ['Idle', 'Run', 'Jump']
+		for animation in animation_types:
+			temp_list = []
+			num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+			for i in range(num_of_frames):
+				img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
+				img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+				temp_list.append(img)
+			self.animation_list.append(temp_list)
+
+		self.image = self.animation_list[self.action][self.frame_index]
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
+
+
+	def update(self):
+		self.update_animation()
+
+
+
+	def move(self, moving_left, moving_right):
+		dx = 0
+		dy = 0
+
+		if moving_left:
+			dx = -self.speed
+			self.flip = True
+			self.direction = -1
+		if moving_right:
+			dx = self.speed
+			self.flip = False
+			self.direction = 1
+
+		if self.jump == True and self.in_air == False:
+			self.vel_y = -17
+			self.jump = False
+			self.in_air = True
+		
+
+		self.vel_y += GRAVITY
+		if self.vel_y > 10:
+			self.vel_y
+		dy += self.vel_y
+
+		for tile in world.obstacle_list:
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				if self.vel_y < 0:
+					self.vel_y = 0
+					dy = tile[1].bottom - self.rect.top
+				elif self.vel_y >= 0:
+					self.vel_y = 0
+					self.in_air = False
+					dy = tile[1].top - self.rect.bottom
+			
+
+
+		self.rect.x += dx
+		self.rect.y += dy
+
+
+
+	def update_animation(self):
+		ANIMATION_COOLDOWN = 100
+		self.image = self.animation_list[self.action][self.frame_index]
+		if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+			self.update_time = pygame.time.get_ticks()
+			self.frame_index += 1
+		if self.frame_index >= len(self.animation_list[self.action]):
+			if self.action == 3:
+				self.frame_index = len(self.animation_list[self.action]) - 1
+			else:
+				self.frame_index = 0
+
+
+
+	def update_action(self, new_action):
+		if new_action != self.action:
+			self.action = new_action
+			self.frame_index = 0
+			self.update_time = pygame.time.get_ticks()
+
+
+
+	def draw(self):
+		screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+class World():
+	def __init__(self):
+		self.obstacle_list = []
+
+	def process_data(self, data):
+		#iterate through each value in level data file
+		for y, row in enumerate(data):
+			for x, tile in enumerate(row):
+				if tile >= 0:
+					img = img_list[tile]
+					img_rect = img.get_rect()
+					img_rect.x = x * TILE_SIZE
+					img_rect.y = y * TILE_SIZE
+					tile_data = (img, img_rect)
+					if tile >= 0 and tile <= 8:
+						self.obstacle_list.append(tile_data)
+					elif tile == 15:#create player
+						player = Soldier('player', x * TILE_SIZE, y * TILE_SIZE, 1.65, 5, 20, 5)
+					elif tile == 20:#create exit
+						exit = Exit(img, x * TILE_SIZE, y * TILE_SIZE)
+						exit_group.add(exit)
+
+		return player
+
+
+	def draw(self):
+		for tile in self.obstacle_list:
+			screen.blit(tile[0], tile[1])
+
+
+
+
+class Exit(pygame.sprite.Sprite):
+	def __init__(self, img, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+
+
+
+exit_group = pygame.sprite.Group()
+
+
+#create empty tile list
+world_data = []
+for row in range(ROWS):
+	r = [-1] * COLS
+	world_data.append(r)
+#load in level data and create world
+with open(f'level{level}_data.csv', newline='') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	for x, row in enumerate(reader):
+		for y, tile in enumerate(row):
+			world_data[x][y] = int(tile)
+world = World()
+player = world.process_data(world_data)
+
+
+
+run = True
+while run:
+
+	clock.tick(FPS)
+
+	#update background
+	draw_bg()
+	#draw world map
+	world.draw()
+
+	player.update()
+	player.draw()
+
+	#update and draw groups
+	exit_group.update()
+	exit_group.draw(screen)
+
+	if player.in_air:
+		player.update_action(2)
+	elif moving_left or moving_right:
+		player.update_action(1)
+	else:
+		player.update_action(0)
+	player.move(moving_left, moving_right)
+
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				moving_left = True
+			if event.key == pygame.K_d:
+				moving_right = True
+			if event.key == pygame.K_w:
+				player.jump = True
+			if event.key == pygame.K_ESCAPE:
+				run = False
+
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				moving_left = False
+			if event.key == pygame.K_d:
+				moving_right = False
+
+
+
+	pygame.display.update()
+
+pygame.quit()
